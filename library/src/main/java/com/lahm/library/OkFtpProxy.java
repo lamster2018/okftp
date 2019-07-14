e4b5a24c200be0352dab14daa0df5015d9c5ce31
lahm <l292538769@gmail.com>
2019-07-14 10:12:12 +0800

    init repository first

---
+++ library/src/main/java/com/lahm/library/OkFtpProxy.java
@@ -0,0 +1,12 @@
+package com.lahm.library;
+
+/**
+ * Project Name:okftp
+ * Package Name:com.lahm.library
+ * Created by lahm on 2019-07-14 09:58 .
+ * <p>
+ * More code see my github https://github.com/lamster2018
+ */
+public interface OkFtpProxy {
+
+}
